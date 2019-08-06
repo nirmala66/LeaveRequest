1770b7978c17190cbd77cbfc16f7786f04aa5bff
nirmala66 <nirmalaa0606@gmail.com>
2019-08-06 15:48:16 +0700

    Add Repository and Interface

--- DataAccess/Context/ApplicationContext.cs
+++ DataAccess/Context/ApplicationContext.cs
@@ -11,10 +11,22 @@ namespace DataAccess.Context
     public class ApplicationContext : DbContext
     {
         public ApplicationContext() : base("LeaveRequest") { }
-        public DbSet<AvailableLeave> AvailableLeave { get; set; }
-        public DbSet<Category> Category { get; set; }
-        public DbSet<Department> Department { get; set; }
-        public DbSet<District> District { get; set; }
-        public DbSet<Division> Division { get; set; }
+        public DbSet<AvailableLeave> AvailableLeaves { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<District> Districts { get; set; }
+        public DbSet<Division> Divisions { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<EmployeeStatus> EmployeeStatus { get; set; }
+        public DbSet<Holiday> Holidays { get; set; }
+        public DbSet<LeaveHistory> LeaveHistories { get; set; }
+        public DbSet<LeaveRequest> LeaveRequests { get; set; }
+        public DbSet<Login> Login { get; set; }
+        public DbSet<Parameter> Parameters { get; set; }
+        public DbSet<Position> Positions { get; set; }
+        public DbSet<Province> Provinces { get; set; }
+        public DbSet<Regency> Regencies { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Village> Villages { get; set; }
     }
 }
