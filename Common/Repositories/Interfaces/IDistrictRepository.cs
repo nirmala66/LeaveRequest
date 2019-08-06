4ebca2a3bee45a468c61795ebda450b6359e626b
ssatyaji <msatyaji31@gmail.com>
2019-08-06 16:59:53 +0700

    Adding Module EmployeeRepository, LoginRepository, RoleRepository, and any file child Employee

---
+++ Common/Repositories/Interfaces/IDistrictRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Models;
+using DataAccess.ViewModels;
+
+namespace Common.Repositories.Interfaces
+{
+    public interface IDistrictRepository
+    {
+        List<District> Get();
+        District Get(int id);
+        List<District> Get(string value);
+        bool Insert(DistrictVM districtVM);
+        //bool Update(int id, DistrictVM districtVM);
+        //bool Delete(int id);
+    }
+}
--- DataAccess/Context/ApplicationContext.cs
+++ DataAccess/Context/ApplicationContext.cs
@@ -11,22 +11,28 @@ namespace DataAccess.Context
     public class ApplicationContext : DbContext
     {
         public ApplicationContext() : base("LeaveRequest") { }
-        public DbSet<AvailableLeave> AvailableLeave { get; set; }
-        public DbSet<Category> Category { get; set; }
-        public DbSet<Department> Department { get; set; }
-        public DbSet<District> District { get; set; }
-        public DbSet<Division> Division { get; set; }
-        public DbSet<Employee> Employee { get; set; }
+        public DbSet<AvailableLeave> AvailableLeaves { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<District> Districts { get; set; }
+        public DbSet<Division> Divisions { get; set; }
+
+        public object Entry(Department get)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DbSet<Employee> Employees { get; set; }
         public DbSet<EmployeeStatus> EmployeeStatus { get; set; }
-        public DbSet<Holiday> Holiday { get; set; }
-        public DbSet<LeaveHistory> LeaveHistory { get; set; }
-        public DbSet<LeaveRequest> LeaveRequest { get; set; }
+        public DbSet<Holiday> Holidays { get; set; }
+        public DbSet<LeaveHistory> LeaveHistories { get; set; }
+        public DbSet<LeaveRequest> LeaveRequests { get; set; }
         public DbSet<Login> Login { get; set; }
-        public DbSet<Parameter> Parameter { get; set; }
-        public DbSet<Position> Position { get; set; }
-        public DbSet<Province> Province { get; set; }
-        public DbSet<Regency> Regency { get; set; }
-        public DbSet<Role> Role { get; set; }
-        public DbSet<Village> Village { get; set; }
+        public DbSet<Parameter> Parameters { get; set; }
+        public DbSet<Position> Positions { get; set; }
+        public DbSet<Province> Provinces { get; set; }
+        public DbSet<Regency> Regencies { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Village> Villages { get; set; }
     }
 }
