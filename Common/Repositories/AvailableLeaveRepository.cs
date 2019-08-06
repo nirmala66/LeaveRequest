4ff4ea8f8393873f349065f80c4e4b2b355d444f
nirmala66 <nirmalaa0606@gmail.com>
2019-08-06 15:58:42 +0700

    Update Repository Name

--- Common/Repositories/AvailableLeaveRepository.cs
+++ Common/Repositories/AvailableLeaveRepository.cs
@@ -12,7 +12,8 @@ using System.Threading.Tasks;
 namespace Common.Repository
 {
     public class AvailableLeaveRepository : IAvailableLeaveRepository
-    {
+    { 
+        //comment
         bool status = false;
         ApplicationContext applicationContext = new ApplicationContext();
 
